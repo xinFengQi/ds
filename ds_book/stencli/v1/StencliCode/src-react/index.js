36e33cf9bf340a77bdf378e2912c6a971ec290b3
tinet-dongfb <dfb3257707086>
2021-01-22 18:13:57 +0800

    增加三个框架运行

---
+++ ds_book/stencli/v1/StencliCode/src-react/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2021-01-22 11:22:29
+ * @LastEditors: dongfb
+ * @LastEditTime: 2021-01-22 15:04:41
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('react')
+);
+
