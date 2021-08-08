a52ab39e9fc9dfd560200e1c2dc0b7f6f92bf5ce
tinet-dongfb <dongfb@ti-net.com.cn>
2021-08-08 21:33:53 +0800

    脚本管理梳理

--- ds-chrome-extension/code_execute_vue/src/pages/homepage/code_manager.vue
+++ ds-chrome-extension/code_execute_vue/src/pages/homepage/code_manager.vue
@@ -1,32 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="code_manager_main">
+    <div class="code_manager_tool">
+      <a-button class="code_manager_tool_btu" type="primary" @click="addList"
+        >增加</a-button
+      >
+      <a-button class="code_manager_tool_btu" type="primary" @click="addList"
+        >保存此次变更</a-button
+      >
+    </div>
+    <div class="code_manager_content">
+      <a-menu
+        style="width: 200px; height: 100%"
+        mode="vertical"
+        v-model:selectedKeys="defaultSelectedKeys"
+        @click="handleClick"
+      >
+        <a-menu-item style="margin: 0" v-for="code in codeList" :key="code.key">
+          {{ code.name }}
+        </a-menu-item>
+      </a-menu>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "codeManager",
+  data() {
+    return {
+      defaultSelectedKeys: [],
+      deate: new Date().getTime(),
+      key: 1,
+      codeList: [
+        {
+          key: "diyi",
+          name: "测试脚本",
+          dev: "这是测试的代码",
+          code: `
+        alert(‘测试的代码’)
+        `,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick: function (ev) {
+      console.log(ev);
+    },
+    addList: function () {
+      this.codeList = [
+        {
+          key: this.deate + ''+ this.key++,
+          name: "测试脚本",
+          dev: "这是测试的代码",
+          code: `
+        alert(‘测试的代码’)
+        `,
+        },
+        ...this.codeList,
+      ];
+    },
+    saveAll: function () {},
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.code_manager_main {
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.code_manager_tool {
+  display: flex;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.code_manager_tool_btu {
+  margin-right: 5px;
 }
 </style>
