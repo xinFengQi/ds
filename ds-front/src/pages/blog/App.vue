5a8c38b0945893a783827282a4712e4b49bfd45c
tinet-dongfb <dongfb@ti-net.com.cn>
2022-02-13 13:34:10 +0800

    压缩及加解密测试

--- ds-front/src/pages/blog/App.vue
+++ ds-front/src/pages/blog/App.vue
@@ -5,6 +5,7 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import { getConfigData } from "@/sevices/gitee.api";
+import poka from "@/sevices/poka.util";
 import store from "./store";
 @Options({
   components: {},
@@ -15,8 +16,17 @@ export default class Home extends Vue {
       if (!data.data) {
         throw "请加入配置文件";
       }
-      store.dispatch('setLayoutData', data.data)
+      store.dispatch("setLayoutData", data.data);
     });
+    const a = poka.zip("121212fsfsdfsfsd分身乏术的");
+    console.log("压缩：", a, a.length);
+    const aceKey = "12121212";
+    const ena = poka.encrypt(a, aceKey);
+    console.log("加密", ena, ena.length);
+    const dac = poka.decrypt(ena, aceKey);
+    console.log("解密", dac, dac.length);
+    const b = poka.unzip(dac);
+    console.log("解压：", b, b.length);
   }
 }
 </script>
